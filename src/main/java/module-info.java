1e0d196e55f21389ad2e48ebb0af2dd689c0abbd
Michael1297 <38764610+Michael1297@users.noreply.github.com>
2026-04-05 15:47:30 +0300

    0.15 release fix module-info exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,14 @@ module io.github.connellite.ExtraLib {
     requires java.sql.rowset;
     requires jdk.management;
     requires static lombok;
+
+    exports io.github.connellite.collections;
+    exports io.github.connellite.concurrent;
+    exports io.github.connellite.exception;
+    exports io.github.connellite.format;
+    exports io.github.connellite.jdbc;
+    exports io.github.connellite.logger;
+    exports io.github.connellite.match;
+    exports io.github.connellite.system;
+    exports io.github.connellite.util;
 }
